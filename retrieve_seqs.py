b11c75c626371600533c3e3d5463a24d90644fac
GLeoni17 <gabriel.leoniduarte@gmail.com>
2026-03-06 17:54:56 -0300

    fix: added NUMBE_SEQUENCES variable to limit quantity of sequences

--- retrieve_seqs.py
+++ retrieve_seqs.py
@@ -1,13 +1,15 @@
 from Bio import Entrez
 
+NUMBER_SEQUENCES = 1000
 Entrez.email = "kiked38026@docsfy.com" 
 
 def retrieve_ref_sequence():
+    print("Downloading reference sequence...")
     handle = Entrez.efetch(db="nucleotide", id="NC_045512.2", rettype="fasta", retmode="text")
     data = handle.read()
     handle.close()
     
-    with open("data/ref.fasta", "w") as f:
+    with open("data/cv_ref.fasta", "w") as f:
         f.write(data)
 
 def retrieve_sequences(qtd: int, term: str = '"Severe acute respiratory syndrome coronavirus 2"[Organism] AND "complete genome"[Title]',):
@@ -18,16 +20,16 @@ def retrieve_sequences(qtd: int, term: str = '"Severe acute respiratory syndrome
     id_list = record["IdList"]
     
     if id_list:
-        print(f"Downloading sequences...")
+        print("Downloading sequences...")
         fetch_handle = Entrez.efetch(db="nucleotide", id=id_list, rettype="fasta", retmode="text")
         data = fetch_handle.read()
         fetch_handle.close()
         
-        with open("data/sequences.fasta", "w") as f:
+        with open("data/cv_sequences.fasta", "w") as f:
             f.write(data)
     else:
         print("None sequence found.")   
     
 if __name__ == "__main__":
     retrieve_ref_sequence()
-    retrieve_sequences(100)
+    retrieve_sequences(NUMBER_SEQUENCES)
